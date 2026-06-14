95f7a8057f28a4b76437955673f25ba8fa490a8a
Pretty Taruvinga <pretty548@gmail.com>
2026-06-14 20:37:30 +0200

    Populate month and year selectors

--- web.mjs
+++ web.mjs
@@ -3,9 +3,45 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-window.onload = function() {
-    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
-}
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+window.onload = function () {
+  const monthSelect = document.getElementById("month-select");
+  const yearSelect = document.getElementById("year-select");
+
+  const currentYear = new Date().getFullYear();
+
+  months.forEach((month, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.textContent = month;
+    monthSelect.appendChild(option);
+  });
+
+  for (let i = currentYear - 10; i <= currentYear + 10; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    yearSelect.appendChild(option);
+  }
+
+  monthSelect.value = new Date().getMonth();
+  yearSelect.value = currentYear;
+
+  console.log(`Loaded ${daysData.length} commemorative days`);
+};
